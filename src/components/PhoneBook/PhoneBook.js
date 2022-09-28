ada3c3150a21eb2d4cf1f88e2011f3707c3738a3
ValikWasT <99086125+ValikWasT@users.noreply.github.com>
2022-09-28 13:43:14 +0300

    add contacts to local storage

--- src/components/PhoneBook/PhoneBook.js
+++ src/components/PhoneBook/PhoneBook.js
@@ -7,14 +7,22 @@ import { ContactListContainer } from 'components/ContactList/ContactList';
 import { FormTitle, ContactTitle, SearchTitle } from './PhoneBooksStyled';
 export class PhoneBook extends React.Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   handleSearch = e => {
     this.setState({ filter: e.currentTarget.value });
